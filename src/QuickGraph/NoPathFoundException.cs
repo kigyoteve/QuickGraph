ab92ec732264b821f960e767d8c7e8e8e7bceeea
Udvari Tibor <udvaritibor95@gmail.com>
2017-04-20 00:28:30 +0200

    Exception types must implement all the required constructors.

--- src/QuickGraph/NoPathFoundException.cs
+++ src/QuickGraph/NoPathFoundException.cs
@@ -1,6 +1,25 @@
-﻿namespace QuickGraph
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace QuickGraph
 {
     public class NoPathFoundException : QuickGraphException
     {
+
+		public NoPathFoundException() {
+
+		}
+
+		public NoPathFoundException(string str) : base(str) {
+
+		}
+
+		public NoPathFoundException(string str, Exception e) : base (str, e) {
+
+		}
+
+		protected NoPathFoundException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) {
+
+		}
     }
 }
