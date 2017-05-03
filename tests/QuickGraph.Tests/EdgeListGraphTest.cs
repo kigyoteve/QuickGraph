09fb4e5d5ac39cca548973974157212156946ada
Ádám <adam95@freemail.hu>
2017-05-03 20:07:50 +0200

    fixed test, now fails where it should

--- tests/QuickGraph.Tests/EdgeListGraphTest.cs
+++ tests/QuickGraph.Tests/EdgeListGraphTest.cs
@@ -87,9 +87,10 @@ namespace QuickGraph.Tests
         public void EdgeListGraphTestContains()
         {
             var g = new EdgeListGraph<int, IEdge<int>>(true, false);
-            g.AddEdge(new Edge<int>(0, 1));
             var e = new Edge<int>(0, 1);
+            g.AddEdge(e);
             Assert.IsTrue(g.ContainsEdge(e));
+            Assert.IsTrue(g.ContainsEdge(new Edge<int>(0, 1)));
             Assert.IsFalse(g.ContainsEdge(new Edge<int>(2, 1)));
             Assert.IsTrue(g.ContainsVertex(0));
             Assert.IsTrue(g.ContainsVertex(1));
