8da042cd0b825ed7578d2ac1ac95b10315635681
Udvari Tibor <udvaritibor95@gmail.com>
2017-05-03 23:54:51 +0200

    SEdgeTest

---
+++ tests/QuickGraph.Tests/SEdgeTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickGraph.Tests {
+
+	[TestClass]
+	public class SEdgeTest {
+
+		[TestMethod]
+		public void SEdgeTestEverything() {
+
+			var e = new SEdge<int>(0, 1);
+			Assert.AreEqual(0, e.Source);
+			Assert.AreEqual(1, e.Target);
+			Assert.AreEqual(String.Format("{0}->{1}", 0, 1), e.ToString());
+
+		}
+	}
+}
